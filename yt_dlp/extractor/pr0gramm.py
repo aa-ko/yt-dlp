5c533a4f53d5a79696065c0a5c0de23c3284633b
Aaron Korte <github@aaronkorte.xyz>
2022-02-22 00:12:21 +0100

    One more test and correct md5 checksums.

--- yt_dlp/extractor/pr0gramm.py
+++ yt_dlp/extractor/pr0gramm.py
@@ -1,21 +1,37 @@
 from .common import InfoExtractor
+from ..utils import *
 
 
 class Pr0grammIE(InfoExtractor):
-    _VALID_URL = r'(?:https?://)?(?:www\.)?pr0gramm\.com/top(?:/[a-zA-Z0-9 üöäß?]+)?/(?P<id>[0-9]+)'
+    _VALID_URL = r'(?:https?://)?(?:www\.)?pr0gramm\.com/top(?:/[a-zA-Z0-9 üöäß?(?:%20)]+)?/(?P<id>[0-9]+)'
     _TESTS = [{
-        'url': 'https://pr0gramm.com/top/4993614',
-        'md5': '96c12f89839979af834b7420136b505f',
+        'url': r'https://pr0gramm.com/top/4993614',
+        'md5': 'd7e32065392421ad45b6f33d440e2eb0',
         'info_dict': {
             'id': '4993614',
             'ext': 'mp4',
             'title': '4993614',
             'thumbnail': r're:^https?://.*\.jpg$',
-            # TODO more properties, either as:
-            # * A value
-            # * MD5 checksum; start the string with md5:
-            # * A regular expression; start the string with re:
-            # * Any Python type (for example int or float)
+            # 'description': '',
+            'timestamp': 1644173081,
+            'upload_date': '20220206',
+            'uploader_id': 14316,
+            'uploader': 'MUSE'
+        }
+    },
+        {
+        'url': r'https://pr0gramm.com/top/Ente gut alles gut/5014992',
+        'md5': '13f4c2a2a2e1ad1303c4577ca84975fb',
+        'info_dict': {
+            'id': '5014992',
+            'ext': 'mp4',
+            'title': '5014992',
+            'thumbnail': r're:^https?://.*\.jpg$',
+            # 'description': '',
+            'timestamp': 1645434037,
+            'upload_date': '20220221',
+            'uploader_id': 326802,
+            'uploader': 'Vermileeyore'
         }
     }]
 
@@ -33,12 +49,13 @@ class Pr0grammIE(InfoExtractor):
         return {
             'id': video_id,
             'title': video_id,  # TODO: There is no title for Pr0gramm videos. Maybe we just use the video id?
-            'description': "video description",  # TODO: There is also no description for Pr0gramm videos...
+            # 'description': "video description",  # TODO: There is also no description for Pr0gramm videos...
 
             # TODO: These should probably be None if the paths are not set at all.
             'url': f"https://vid.pr0gramm.com/{image_path}",
             'thumbnail': f"https://thumb.pr0gramm.com/{thumbnail_path}",
 
+            'uploader': api_response.get('user'),
             'uploader_id': api_response.get('userId'),
 
             # This does not work right now, because the page does not contain the user if there is no JS to load the content.
